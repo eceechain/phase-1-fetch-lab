fb2d22ddfbaa639b3f19b2a1a2c630691f435041
ecee <you@example.com>
2023-10-05 14:47:39 +0300

    Done

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp =>resp.json())
+  .then((books) => {
+    books.forEach((book) => {
+      const renderBook1 = renderBooks(book)
+    })
+  }
+)}
 
-function renderBooks(books) {
+function renderBooks(book) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const renderBook1 = document.createElement('h2');
+    renderBook1.innerHTML = book.name;
+    main.appendChild(renderBook1);
+  return main;
 }
+renderBooks();
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks(fetch('https://anapioficeandfire.com/api/books')
-  .then(resp =>resp.json())
-  .then(books => renderBooks(books)));
+  fetchBooks();
+
 });
